58a9e27d27cde04e349ea0484a3a8d60f4fae368
Nazi-Nigger <avik111191@hotmail.com>
2015-01-13 18:23:54 +0530

    Update plugin2.cs

--- plugin2.cs
+++ plugin2.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Runtime.Serialization;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
 using Microsoft.Xrm.Sdk.Messages;
@@ -19,14 +19,26 @@ namespace plugin2
             IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
             IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
             IOrganizationService service = factory.CreateOrganizationService(context.UserId);
-            nazi_b_22_entity b_22= (nazi_b_22_entity)context.InputParameters["Target"];
-            nazi_b_23_entity b_23= new nazi_b_23_entity();
-            b_23.nazi_name=b_22.nazi_name;
-            b_23.nazi_at3=b_22.nazi_X;
-            b_23.OwnerId=b_22.OwnerId;
+            Entity b_22= (Entity)context.InputParameters["Target"];
+            ColumnSet c= new ColumnSet();
+            c.AllColumns=true;
+            Guid g = new Guid("{0314349B-F69A-E411-80E3-80C16E644B9A}");
+            //Entity temp = service.Retrieve("nazi_b_22_entity",g,c); ;
 
+           // b_22["statecode"] = "inactive";
+            nazi_b_23_entity b_23= new nazi_b_23_entity();
+            b_23.nazi_name=(string)b_22["nazi_name"];
+            //b_23.nazi_at3=(string)b_22["nazi_x"];
+            b_23.OwnerId=(EntityReference)b_22["ownerid"];
+            var p = b_22["statecode"];
+            var q = b_22["statuscode"];
+           // b_22.statecode = new OptionSetValue((int));
             Guid b_23_id = service.Create(b_23);
-            service.
+            b_22["nazi_name"] = "changed";
+            b_22["statecode"] = new OptionSetValue(1);
+            service.Update(b_22);
+
+            
             
 
         }
