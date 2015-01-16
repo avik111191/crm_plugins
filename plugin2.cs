bea512e111be6fc67ff5c27487c2f76b596fd69b
Nazi-Nigger <avik111191@hotmail.com>
2015-01-16 11:40:08 +0530

    Update plugin2.cs

--- plugin2.cs
+++ plugin2.cs
@@ -2,10 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
 using System.Runtime.Serialization;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
 using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Crm.Sdk.Messages;
+
+
 
 using crm_ddl_namespace;
 
@@ -13,34 +17,64 @@ namespace plugin2
 {
     public class Class1:IPlugin
     {
+        Entity b_22;
+        nazi_b_23_entity b_23 = new nazi_b_23_entity();
+        ColumnSet c= new ColumnSet(true);
+        IPluginExecutionContext context;
+        IOrganizationServiceFactory factory;
+        IOrganizationService service;
+        SetStateRequest setstate = new SetStateRequest();
+        IServiceProvider serviceProvider_;  
         public void Execute(IServiceProvider serviceProvider)
         {
-            //ITracingService trace;
-            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
-            IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
-            IOrganizationService service = factory.CreateOrganizationService(context.UserId);
-            Entity b_22= (Entity)context.InputParameters["Target"];
-            ColumnSet c= new ColumnSet();
-            c.AllColumns=true;
-            Guid g = new Guid("{0314349B-F69A-E411-80E3-80C16E644B9A}");
-            //Entity temp = service.Retrieve("nazi_b_22_entity",g,c); ;
 
-           // b_22["statecode"] = "inactive";
-            nazi_b_23_entity b_23= new nazi_b_23_entity();
-            b_23.nazi_name=(string)b_22["nazi_name"];
-            //b_23.nazi_at3=(string)b_22["nazi_x"];
-            b_23.OwnerId=(EntityReference)b_22["ownerid"];
-            var p = b_22["statecode"];
-            var q = b_22["statuscode"];
-           // b_22.statecode = new OptionSetValue((int));
-            Guid b_23_id = service.Create(b_23);
-            b_22["nazi_name"] = "changed";
-            b_22["statecode"] = new OptionSetValue(1);
-            service.Update(b_22);
-
-            
-            
+             serviceProvider_ = serviceProvider;
+             initiate(); 
+             cal_create();
+             cal_change_state();
+         
 
         }
+
+        private async void initiate()
+        {
+            await init_();
+        }
+
+        private async System.Threading.Tasks.Task init_()
+        {
+            context = (IPluginExecutionContext)serviceProvider_.GetService(typeof(IPluginExecutionContext));
+            factory = (IOrganizationServiceFactory)serviceProvider_.GetService(typeof(IOrganizationServiceFactory));
+            service = factory.CreateOrganizationService(context.UserId);
+            b_22 = (Entity)context.InputParameters["Target"];
+            b_23.nazi_name = (string)b_22["nazi_name"];
+            b_23.OwnerId = (EntityReference)b_22["ownerid"];
+        }
+
+        private async void cal_change_state()
+        {
+            await cal_change_stat();
+        }
+        public async System.Threading.Tasks.Task cal_change_stat()
+        {
+            setstate.EntityMoniker = new EntityReference(b_22.LogicalName,b_22.Id);
+            //setstate.State = new OptionSetValue((int)IncidentState.Canceled);
+            ////setstate.Status=new OptionSetValue()
+            setstate.State = new OptionSetValue(1);
+            setstate.Status = new OptionSetValue(2);
+            service.Execute(setstate);
+        }
+
+        private async void cal_create()
+        {
+            await create_();
+        }
+
+        public async System.Threading.Tasks.Task create_()
+        {
+
+           Guid b_23_id = service.Create(b_23);
+        }
+
     }
 }
